12d688f03636494e45bb62e39cdc634acbb7d020
Isha Ray <isharay009@gmail.com>
2026-04-27 01:44:24 +0530

    Project

--- 30/script.js
+++ 30/script.js
@@ -1,5 +1,8 @@
-const colorCodeContainer = document.getElementById("color-code")
+const colorCodeContainer = document.getElementById("color-code");
+const optionContainer = document.getElementById("options-container");
+const scoreContainer = document.getElementById("score");
 let randomColor = null;
+let score = 0;
 
 function generateRandomNumberBetween(min, max){
     return min + Math.floor(Math.random() * (max-min + 1))
@@ -13,9 +16,40 @@ function generateRandomColorRGB(){
     return `rgb(${red}, ${green}, ${blue})`;
 }
 
+function incrementScore(){
+    score += 1;
+    scoreContainer.innerText = score;
+
+}
+
+function validateResult(el){
+    const selectedColor = el.target.style.backgroundColor;
+    if(selectedColor === randomColor) {
+        incrementScore();
+    } else {
+        score = 0;
+    }
+    window.localStorage.setItem("score", score);
+    startGame();
+}
+
+
 function startGame(){
+    score = Number(window.localStorage.getItem('score')) ?? 0;
+    scoreContainer.innerText = score;
+    optionContainer.innerHTML = null;
     randomColor = generateRandomColorRGB();
     colorCodeContainer.innerText = randomColor;
+
+    const ansIndex = generateRandomNumberBetween(0, 5);
+
+    for(let i = 0; i < 6; i++){
+        const div = document.createElement('div'); 
+        div.addEventListener('click', validateResult);
+        div.style.backgroundColor = 
+        i === ansIndex ? randomColor : generateRandomColorRGB();
+        optionContainer.append(div);
+    }
 }
 
 window.addEventListener("load", () => startGame())
